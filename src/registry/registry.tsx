762d94365e184b15548aa1340e746facbfcbf3ae
korenyugin <korenyugin@satel.org>
2026-03-02 17:57:05 +0300

    adjust table styles

--- src/registry/registry.tsx
+++ src/registry/registry.tsx
@@ -1,6 +1,5 @@
 import {App, Button, Checkbox, Flex, Input } from "antd";
 import {PlusOutlined, RedoOutlined} from "@ant-design/icons";
-import TableRegistryAll from "./model/TableRegistryAll.tsx";
 import CreateNewRegisterDataModal from "./model/modals/CreateNewRegisterDataModal.tsx";
 import {useModals, useRegister} from "../store.ts";
 import './style/registry.modules.css'
@@ -45,47 +44,12 @@ function RegistryPage() {
     setItemsByQuerySelected(itemsByQuery);
   }, [querySelected, selectedItems]);
 
-  // useEffect(() => {
-  //   fetchSelectedItems();
-  // }, [itemsByQueryUnselected]);
-
   const fetchItems = (startIndex = 0, lastIndex = 20) => {
     const newElements = items.slice(startIndex, lastIndex);
     const newItems = [..._items, ...newElements];
     setItems(newItems);
     return newItems;
   }
-
-  // const syncSelection = () => {
-  //   const selected = itemsByQueryUnselected.filter(it => it.isSelected);
-
-  //   const res = _items.map(it => {
-  //     return {
-  //       ...it,
-  //       isSelected: selected.includes(it.id)
-  //     }
-  //   });
-
-  //   return res;
-  // }
-
-  // const fetchSelectedItems = () => {
-  //   const newItems = itemsByQueryUnselected.filter(item => item.isSelected);
-  //   setSelectedItems(newItems);
-  //   return newItems;
-  // }
-
-  const setQueryEvent = (e) => {
-    setQueryUnselected(
-      e.target.value
-    );
-  }
-
-  // const setQueryEvent = (e) => {
-  //   setQueryUnselected(
-  //     e.target.value
-  //   );
-  // }
 
   const searchByQuery = (query: string, items: any[]) => {
     const normalizeText = (text: string) => (text ?? '').trim().toLowerCase();
@@ -108,16 +72,6 @@ function RegistryPage() {
 
     setItems(newItems);
   }
-
-  // const toggleSelectionRight = (item) => {
-  //   const newValue = !item.isSelected;
-
-  //   const newItems = itemsByQuerySelected.map(e =>
-  //     e.id === item.id ? { ...e, isSelected: newValue } : e
-  //   );
-
-  //   setItemsByQuerySelected(newItems);
-  // }
 
   return (
       <div className="main-page__container">
@@ -147,7 +101,6 @@ function RegistryPage() {
                     dataLength={_items?.length}
                     next={() => fetchItems(_items?.length || 0, (_items?.length + 20))}
                     hasMore={true}
-                    // loader={<h4>Loading...</h4>}
                     loader={<div></div>}
                     scrollableTarget="scrollable-div"
                   >
@@ -205,7 +158,6 @@ function RegistryPage() {
                     dataLength={selectedItems?.length}
                     next={() => fetchItems(_items?.length || 0, (_items?.length + 20))}
                     hasMore={true}
-                    // loader={<h4>Loading...</h4>}
                     loader={<div></div>}
                     scrollableTarget="scrollable-div-2"
                   >
@@ -231,7 +183,6 @@ function RegistryPage() {
                             { itemsByQuerySelected.map(item => {
                               return (
                                 <tr key={item.id}>
-                                  {/* <td> { String(item.isSelected) } </td> */}
                                   <td>
                                     { <Checkbox 
                                         checked={item.isSelected}
@@ -244,7 +195,6 @@ function RegistryPage() {
                                 </tr>
                               )
                             }) }
-                          {/* </InfiniteScroll> */}
                       </tbody>
                     </table>
                   </div>
